9c3e29a19a7ef411a91d660a5a44d9c80b1eb791
Jose <Jrivera6869@yahoo.com>
2020-02-28 19:03:26 -0500

    repo up

---
+++ api/api-routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const AuthRouter = require('../api/routes/auth/auth-route');
+const restricted = require('../api/routes/auth/restricted-middleware');
+const UserRouter = require('../api/routes/users/user-route');
+
+router.get('/', (req, res) => {
+  res.status(200).json({ welcome: 'API router' });
+});
+
+router.use('/auth', AuthRouter);
+router.use(
+  '/users',
+  restricted,
+  checkRole('user', 'owner', 'web26'),
+  UserRouter
+);
+
+module.exports = router;
+
+function checkRole(...roles) {
+  return (req, res, next) => {
+    console.log(req.decodedToken);
+    if (
+      req.decodedToken &&
+      req.decodedToken.department &&
+      roles.includes(req.decodedToken.department.toLowerCase())
+    ) {
+      next();
+    } else {
+      res.status(403).json({ message: `Don't have access!` });
+    }
+  };
+}
---
+++ knexfile.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  testing: {
+    client: 'pg',
+    connection: 'postgresql://localhost/',
+
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
