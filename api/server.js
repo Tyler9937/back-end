f984e730350b712af16410ce192468a59a05b08a
Jose <Jrivera6869@yahoo.com>
2020-03-02 10:54:23 -0500

    vinnie2

--- api/server.js
+++ api/server.js
@@ -17,22 +17,11 @@ server.use('/api', ApiRouter);
 // server.get('/', (req, res) => {
 //   res.status(200).json({ message: 'ITS WORKING!!!' });
 // });
-// server.get('/', (req, res) => {
-//   res.sendFile('../index.html');
-// });
 
-// if (process.env.NODE_ENV === 'production') {
-//   server.use(express.static('BACK-END/imgs'));
-// }
-server.use(express.static(path.join(__dirname)));
+server.use(express.static(path.join(__dirname, '../imgs')));
 
 server.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, '../imgs', 'index.html'));
-
-  // res.sendFile(
-  //   'C:/Users/Jrive/Desktop/Lambda projects/back-end/imgs/index.html'
-  // );
-  //__dirname : It will resolve to your project folder.
 });
 
 module.exports = server;
